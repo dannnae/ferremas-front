ec2705f328a7bd907fd68c3e55c99d4fd32226a7
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-15 18:11:10 -0400

    test webpay

--- src/app/pages/venta/venta.component.ts
+++ src/app/pages/venta/venta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DjangoService } from 'src/app/services/django.service';
 
 @Component({
   selector: 'app-venta',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./venta.component.css']
 })
 export class VentaComponent {
+  tokenPago: string | undefined; 
 
+  constructor(private djangoservice: DjangoService) { }
+
+  crearBoleta() {
+    const boletaData = {
+      direccion_entrega: 'asd', 
+      tienda_entrega: '1', 
+      usuario: '1', 
+      valor_total: 111
+    };
+
+    this.djangoservice.crearBoleta(boletaData).subscribe(
+      (response) => {
+        console.log('respuesta de creación de boleta:', response);
+        this.djangoservice.confirmarPago(response.url, { 'token_ws': response.token }).subscribe(
+          (response) => {
+            console.log(response)
+          },
+          (error) => {
+            console.log(error)
+          }
+        )
+      },
+      (error) => {
+        console.error('Error al crear la boleta:', error);
+      }
+    );
+  }
+      
 }
+
