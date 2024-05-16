62cc827dc34acb03249b79883be3ab2e1bc258b9
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-15 20:25:27 -0400

    login basico

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DjangoService } from '../../services/django.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private djangoService: DjangoService, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.djangoService.login(this.loginForm.value).subscribe(
+        (response: any) => {
+          console.log('Login exitoso:', response);
+          localStorage.setItem('access_token', response.access);
+          localStorage.setItem('refresh_token', response.refresh);
+          this.router.navigate(['/home']);
+        },
+        (error: any) => {
+          console.error('Error en el login:', error);
+        }
+      );
+    }
+  }
 }
+
