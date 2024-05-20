b830e1faaaa7ea626927600b28927d69f36d3ea9
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-20 17:37:11 -0400

    agregado CarritoService

--- src/app/pages/venta/venta.component.ts
+++ src/app/pages/venta/venta.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { DjangoService } from 'src/app/services/django.service';
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-venta',
   templateUrl: './venta.component.html',
   styleUrls: ['./venta.component.css']
 })
-export class VentaComponent {
+export class VentaComponent implements OnInit{
   tokenPago: string | undefined; 
+  carrito: any;
+  constructor(private djangoservice: DjangoService, private router: Router, private carritoService: CarritoService) { }
 
-  constructor(private djangoservice: DjangoService, private router: Router) { }
+  ngOnInit(): void {
+    this.carritoService.getCarrito().subscribe(
+      (response) => {
+        this.carrito = response
+        console.log(this.carrito)
+      }
+    )
+  }
 
   crearBoleta() {
-    const boletaData = {
-      direccion_entrega: 'asd', 
-      tienda_entrega: '1', 
-      usuario: '1', 
-      valor_total: 111
-    };
-
-    this.djangoservice.crearBoleta(boletaData).subscribe(
+    this.djangoservice.crearBoleta(this.carrito.id).subscribe(
       (response) => {
         console.log('respuesta de creación de boleta:', response);
         const navigationExtras: NavigationExtras = {
