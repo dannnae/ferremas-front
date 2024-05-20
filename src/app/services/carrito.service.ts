9a39988562e8e4e6c58ecbb3e33c37562d72f3bb
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-20 17:37:47 -0400

    funcion getCarrito

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,8 @@ export class CarritoService {
 
   constructor(private http: HttpClient) { }
 
+  getCarrito(): Observable<any> {
+    return this.http.get<any>(`${this.apiURL}/boleta/get_carrito/`);
+  }
   }
 
