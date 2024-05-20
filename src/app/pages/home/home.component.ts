22a9f9db0cef8c9359af6aa1e0dda7f542c21d0b
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-20 17:35:08 -0400

    agregado CarritoService

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private carritoService: CarritoService) {}
+  ngOnInit(): void {
+      this.carritoService.getCarrito().subscribe(
+        (response) => {
+          console.log(response)
+        }
+      )
+  }
 }
