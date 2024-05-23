fb789143aab07c6de6b9fc9f97d23b8b977dafc2
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-23 18:06:42 -0400

    modificado diseno

--- src/app/pages/venta/venta.component.ts
+++ src/app/pages/venta/venta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { DjangoService } from 'src/app/services/django.service';
 import { CarritoService } from 'src/app/services/carrito.service';
@@ -8,35 +8,61 @@ import { CarritoService } from 'src/app/services/carrito.service';
   templateUrl: './venta.component.html',
   styleUrls: ['./venta.component.css']
 })
-export class VentaComponent implements OnInit{
-  carrito: any;
-  constructor(private djangoservice: DjangoService, private router: Router, private carritoService: CarritoService) { }
+export class VentaComponent implements OnInit {
+  carrito: any = { items: [] }; 
+  selectedOption: string = 'Debito o credito';
+  totalPedido: number = 0;
+
+  constructor(
+    private djangoservice: DjangoService, 
+    private router: Router, 
+    private carritoService: CarritoService,
+    private cdr: ChangeDetectorRef 
+  ) {}
 
   ngOnInit(): void {
     this.carritoService.getCarrito().subscribe(
       (response) => {
-        this.carrito = response
-        console.log(this.carrito)
+        console.log('Carrito Response:', response);
+        this.carrito = response;
+        this.calcularTotalPedido();
+        this.cdr.detectChanges(); 
+      },
+      (error) => {
+        console.error('Error fetching carrito:', error);
       }
-    )
+    );
+  }
+
+  onRadioChange(value: string) {
+    this.selectedOption = value;
   }
 
   crearBoleta() {
-    this.djangoservice.crearBoleta(this.carrito.id).subscribe(
-      (response) => {
-        console.log('respuesta de creación de boleta:', response);
-        const navigationExtras: NavigationExtras = {
-          state: {
-            htmlContent: response
-          }
-        };
-        this.router.navigate(['/webpay'], navigationExtras);
-      },
-      (error) => {
-        console.log(error)
-      }
-    )
+    if (this.selectedOption === 'Debito o credito') {
+      this.djangoservice.crearBoleta(this.carrito.id).subscribe(
+        (response) => {
+          console.log('respuesta de creación de boleta:', response);
+          const navigationExtras: NavigationExtras = {
+            state: {
+              htmlContent: response
+            }
+          };
+          this.router.navigate(['/webpay'], navigationExtras);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.log('Pago por transferencia seleccionado');
+    }
   }
-      
+  calcularTotalPedido() {
+    this.totalPedido = 0;
+    for (let item of this.carrito.pedidos) {
+      this.totalPedido += item.valor_total;
+    }
+  }
 }
 
